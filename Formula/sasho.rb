76c8adcb3312ecc16b8dc30e9b71564403ccf7a6
pineapple <45973943+kleidiCode@users.noreply.github.com>
2026-04-21 22:36:09 -0700

    Update formula with release SHA

--- Formula/sasho.rb
+++ Formula/sasho.rb
@@ -2,7 +2,7 @@ class Sasho < Formula
   desc "Native macOS window manager with keyboard shortcuts and trackpad gestures"
   homepage "https://github.com/kleidiCode/sasho"
   url "https://github.com/kleidiCode/sasho/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "Apache-2.0"
   version "0.1.0"
 
